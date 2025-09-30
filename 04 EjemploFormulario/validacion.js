9190e8242538e11c1f799f42df5829cc5e65bba0
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-09-30 00:31:42 -0600

    Correccion de error

--- 04 EjemploFormulario/validacion.js
+++ 04 EjemploFormulario/validacion.js
@@ -1,61 +1,89 @@
 /*
-enma script
-Javascript es un lenguaje multiparadigma
-Acepta la programación funcional, estructurada, POO, Eventos
-
-Dentro de Js, no existe el typado de variables; int, string, etc
-Solo existen 3 tipos de varibles de acuerdo al estandar ES6: let, const y var
+JavaScript es un lenguaaje multiparadigma
+Acepta la programacion funcional, estructurada, POO, Eventos
+Dentro de JS, no existe el typado de variables (int, string, float, etc)
+Solo existen 3 tipos de variables de acuerdo al estandar ES6
+VAR, LET y CONST
 */
 
 function validar(formulario) {
- 
-    //quiero validar que el campo nombre acepte más de 3 caracteres
-
-    if (formulario.nombre.value.length < 4) {
-
-        alert("Por favor, escribe más de 3 caracteres en el campo nombre");
+    //quiero validar que el campo nombre acepte mas de 3 caracteres
+    if (formulario.Nombre.value.length < 4) {
+        alert("Porfavor escribe más de 3 caracteres en el campo nombre");
         formulario.nombre.focus();
         return false;
-    
-    
+    }
+    if (formulario.Edad.value > 100) {
+        alert("Porfavor escribe una edad válida");
+        formulario.Edad.focus();
+        return false;
+    }
+    if (formulario.Edad.value < 0) {
+        alert("Porfavor escribe una edad válida");
+        formulario.Edad.focus();
+        return false;
     }
 
+    //validacio para unicamente letras
+    var checkStr = formulario.Nombre.value;
+    alert(checkStr);
 
+    var abcOk = "QWERTYUIOPASDFGHJKLÑZXCVBNM" + "qwertyuiopasdfghjklñzxcvbnm";
 
-    //validacion unicamente letras
-    var checStr = formulario.nombre.value;
-    alert(checStr);
+    var allValido = true;
 
-    var abcOK = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ '; 
-    var aliValido = true;
-    //tememos que comparar la cadena de nombre vs abc
+    //tenemos que comparar la cadena de nombre vs abc
 
-    for (var i = 0; i < checStr.length; i++) {
-        var caracteres = checStr.charAt(i);
-        for (var j = 0; j < abcOK.length; j++) {
-            if (caracteres == abcOK.charAt(j)) break;
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        for (var j = 0; j < abcOk.length; j++) {
+            if (caracteres == abcOk.charAt(j)) {
+                break;
+            }
         }
-        if (j == abcOK.length) {
-            aliValido = false;
+        if (j == abcOk.length) {
+            allValido = false;
             break;
         }
-    
-     }
-     if (!aliValido) { alert("Escriba unicamente letras en el campo nombre"); formulario.nombre.focus(); return false;
     }
 
-     if (!aliValido) { alert("Escriba unicamente digitos en el campo nombre"); formulario.edad.focus(); return false;
+    
+    if(!allValido){
+        alert("Escribe unicamente letras en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+    
+    var checkStr = formulario.Edad.value;
+    alert(checkStr);
 
+    var abcOk = "1234567890" + "qwertyuiopasdfghjklñzxcvbnm" + " ";
+
+    var allValido = true;
+
+    //tenemos que comparar la cadena de nombre vs abc
+
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        for (var j = 0; j < abcOk.length; j++) {
+            if (caracteres == abcOk.charAt(j)) {
+                break;
+            }
+        }
+        if (j == abcOk.length) {
+            allValido = false;
+            break;
+        }
+    }
+
+    if(!allValido){
+        alert("Escribe unicamente digitos en el campo edad");
+        formulario.Edad.focus();
+        return false;
     }
 
     //vamos a crear una funcion de una expresion regular para validar el correo electronico
     //texto.texto@texto.texto
 
-    var b = /^[^@\s]+[^@\.\s]+(\.[^@\.\s]+)+$/; 
-    var txt = formulario.correo.value;
-
-alert("Email" + (b.test(txt)? " " : " no válido") + ("valido"));
-
-return b.test;
-//agregar funcion para su nombre con espacios: Jaime Minor Gomez, y una edad con negativos, 23456789
+   
 }
